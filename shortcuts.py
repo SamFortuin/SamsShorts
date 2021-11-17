ff3fe469b03a5e11ac2158155a30b18a0f539824
Sam Fortuin <99059050@mydavinci.nl>
2021-11-17 11:07:51 +0100

    added intConvert

--- shortcuts.py
+++ shortcuts.py
@@ -1,4 +1,15 @@
 import time, os, sys
+# For relative imports to work in Python 3.6
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# from pathlib import Path
+# print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+
+#When testing new function put it here
+def _test():
+    pass
+
+if __name__ == '__main__':
+    _test()
 
 #print slow from sabastion on stackoverflow, good lad
 def print_slow(str):
@@ -26,3 +37,14 @@ def listPrint(List):
   for i in range(roomCount):
     time.sleep(0.12)
     print(List[i])
+
+def intConvert(num):
+    numConvert = False
+    for i in range(10):
+        if str(i) in num:
+            numConvert = True
+    if numConvert == True:
+        return int(num)
+    else:
+        print('Niet een getal.')
+        exit()
